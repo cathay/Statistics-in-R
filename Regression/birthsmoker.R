104638db7984d1791f860990c09f160ec016241c
Thay Huynh <thayhuynh@HPVN-064L.local>
2017-02-11 23:57:30 +0700

    Add regression for birth smoker analysis. This is the first example for categorical predictors:

---
+++ Regression/birthsmoker.R
@@ -0,0 +1,17 @@
+rawData <-  read.table(paste(getwd(),"/birthsmoker.data",sep = ""), header = TRUE, stringsAsFactors=FALSE)
+
+# Transform catogirical variables to numeric
+rawData$Smoke[rawData$Smoke == 'yes'] <- 1
+rawData$Smoke[rawData$Smoke == 'no'] <- 0
+attach(rawData)
+Smoke <- as.numeric(Smoke)
+Smoke
+
+model <- lm(Wgt ~ Smoke + Gest)
+summary(model)
+
+#Calculate CI for Smoke -> 
+# We can be 95% confident that the mean birth weight of smoking mothers is between 158.7 and 330.4 grams less than 
+#the mean birth weight of non-smoking mothers, regardless of the length of gestation. 
+#It is up to the researchers to debate whether or not the difference is a meaningful difference.
+Smoke_CI <- confint(model, "Smoke", level = 0.95)
